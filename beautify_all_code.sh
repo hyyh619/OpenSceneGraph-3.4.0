8aaf6c703d3453b9fa65c8e1c2592a85e4afa3dd
Huang Ying <hyyh619@hotmail.com>
2016-05-20 16:56:21 +0800

    OSG-3.4.0: add new beautify command.
    By: ying.huang.

--- beautify_all_code.sh
+++ beautify_all_code.sh
@@ -1,2 +1,4 @@
 find . -name "*.c" -o -name "*.cpp" -o -name "*.h" -o -name "*.hpp" -o -name "*.m" > source.files 
 uncrustify -c ../../tools/hy_c_c++_for_osx.cfg -F source.files --no-backup
+find ./include -name "*" > header.files
+uncrustify -c ../../tools/hy_c_c++_for_osx.cfg -F header.files --no-backup
