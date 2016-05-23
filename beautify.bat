b0ca354e10ca3f2956222f3a049cbc46f476d6c9
Huang Ying <hyyh619@hotmail.com>
2016-05-23 15:16:46 +0800

    OSG-3.4.0: fix android build error.
    By: ying.huang.

--- beautify.bat
+++ beautify.bat
@@ -1,4 +1,4 @@
-find . -name "*.c" -o -name "*.cpp" -o -name "*.h" -o -name "*.hpp" -o -name "*.m" > source.files 
+REM find . -name "*.c" -o -name "*.cpp" -o -name "*.h" -o -name "*.hpp" -o -name "*.m" > source.files 
 ..\..\..\..\tools\uncrustify-0.62.0-g5987f22-win32\uncrustify.exe -c "..\..\..\tools\hy_c_c++.cfg" --no-backup -F source.files
-find ./include -name "*" > header.files
-..\..\..\..\tools\uncrustify-0.62.0-g5987f22-win32\uncrustify.exe -c "..\..\..\tools\hy_c_c++.cfg" --no-backup -F header.files
+REM find ./include -name "*" > header.files
+REM ..\..\..\..\tools\uncrustify-0.62.0-g5987f22-win32\uncrustify.exe -c "..\..\..\tools\hy_c_c++.cfg" --no-backup -F header.files
