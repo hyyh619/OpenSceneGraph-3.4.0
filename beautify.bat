fdfb9b6c3f4a96d1faba19ee0e997532480fdcf8
Huang Ying <hyyh619@hotmail.com>
2016-05-23 15:24:32 +0800

    OSG-3.4.0: update beautify command.
    By: ying.huang.

--- beautify.bat
+++ beautify.bat
@@ -1,4 +1,4 @@
 REM find . -name "*.c" -o -name "*.cpp" -o -name "*.h" -o -name "*.hpp" -o -name "*.m" > source.files 
-..\..\..\..\tools\uncrustify-0.62.0-g5987f22-win32\uncrustify.exe -c "..\..\..\tools\hy_c_c++.cfg" --no-backup -F source.files
+..\..\..\tools\uncrustify.exe -c "..\..\..\tools\hy_c_c++.cfg" --no-backup -F source.files
 REM find ./include -name "*" > header.files
-REM ..\..\..\..\tools\uncrustify-0.62.0-g5987f22-win32\uncrustify.exe -c "..\..\..\tools\hy_c_c++.cfg" --no-backup -F header.files
+REM ..\..\..\tools\uncrustify.exe -c "..\..\..\tools\hy_c_c++.cfg" --no-backup -F header.files
